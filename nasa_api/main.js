08e042baf0985d004c57ab56ef0f974da0b59242
Agnieszka Skubiszewska <marszmelow@gmail.com>
2021-11-18 19:54:33 +0100

    Nasa Api Connection
    FIrst api connection

---
+++ nasa_api/main.js
@@ -0,0 +1,46 @@
+const btn = document.querySelector('button');
+const input = document.querySelector("input");
+const passwords = ['jedEN', 'DwA'];
+const messages = ["super", "działa!"]
+let wrrong = 'welcome'
+const url = 'www.nasa.com'
+
+
+
+const showMessage = (e) => {
+    div.textContent = " "
+    const text = e.target.value
+   
+    console.log(text)
+
+ if (input != null){
+     div.textContent= wrrong
+    
+ }
+
+}
+
+ async function sendAPIRequest(){
+     let  API_KEY = '4rCD5fB01JP5acQc0XaZv2pvuUizl4zIuYNMqoGo'
+     console.log('click')
+let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
+console.log(response)
+   let data = await response.json()
+       console.log(data)
+   useApiData(data)
+}
+function useApiData(data){
+
+document.querySelector('#content').innerHTML += data.explanation
+
+document.querySelector('#content').innerHTML += `<img src = "${data.url}">`
+
+
+
+}
+
+
+
+
+input.addEventListener("input", showMessage)
+btn.addEventListener('click', sendAPIRequest)
